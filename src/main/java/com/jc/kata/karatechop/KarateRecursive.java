fb9488c46d619cbf78a1d37dc4a4e39bd304314d
Jose Carlos <josecarlosmartinez@gmail.com>
2019-01-20 19:33:39 +0100

    Add recursive algorithm as valid solution

--- src/main/java/com/jc/kata/karatechop/KarateRecursive.java
+++ src/main/java/com/jc/kata/karatechop/KarateRecursive.java
@@ -3,8 +3,20 @@ package com.jc.kata.karatechop;
 public class KarateRecursive implements Karate {
 
 	public int chop(int value, int[] array) {
-		return -1;
+		return chopRecursive(value, array, 0, array.length-1);
 	}
 	
-
+	public int chopRecursive(int value, int[] array, int leftIndex, int rightIndex) {
+		if(leftIndex > rightIndex) {
+			return -1;
+		}
+		int middle = (rightIndex + leftIndex)/2;
+		if (value == array[middle]) {
+			return middle;
+		}
+		else if(value > array[middle]) {
+			return chopRecursive(value, array, middle+1, rightIndex);
+		}
+		return chopRecursive(value, array, leftIndex, middle-1);
+	}
 }
